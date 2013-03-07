e9835159a0e484ef8646307eeddc8540a5ce5486
Dmitry Dudalov <dmitry@host-192-168-1-25.zuzino.net>
2013-03-07 20:44:11 +0400

    New things and tables for notifications remap.

---
+++ xml_parser/sql_helpers/dropper_gun.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION dropper_gun(IN _schema TEXT, IN _parttionbase TEXT) 
+RETURNS void 
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    row     record;
+BEGIN
+    FOR row IN 
+        SELECT
+            table_schema,
+            table_name
+        FROM
+            information_schema.tables
+        WHERE
+            table_type = 'BASE TABLE'
+        AND
+            table_schema = _schema
+        AND
+            table_name ILIKE (_parttionbase || '%')
+    LOOP
+        EXECUTE 'DROP TABLE ' || quote_ident(row.table_schema) || '.' || quote_ident(row.table_name) ||' cascade;';
+        RAISE INFO 'Dropped table: %', quote_ident(row.table_schema) || '.' || quote_ident(row.table_name);
+    END LOOP;
+END;
+$$;
+
+-- Execute like 
+
+--select dropper_gun('public','_notif');
